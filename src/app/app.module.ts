93d6698ba5501eeb679757532a60e829f9b5c319
Adam Hansen <adamlars90@gmail.com>
2017-03-10 22:37:50 +0100

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';;
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Angular2FontawesomeModule,
+    [MaterialModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
